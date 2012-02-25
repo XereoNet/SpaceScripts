0d498eefacd7245df4321970983a23b916b7a830
teh-al3x <alex.atomic@hotmail.de>
2012-02-25 23:12:39 +0100

    Anti bumblebee security system added.

--- install.sh
+++ install.sh
@@ -19,6 +19,14 @@ then
 #	exit 1
 fi
 
+if [ "$(pwd)" = '/' ] || [ "$(pwd)" = '/home' ] || pwd | grep '^/etc' > /dev/null || pwd | grep '^/bin' > /dev/null || pwd | grep '^/boot' > /dev/null || pwd | grep '^/lib' > /dev/null || pwd | grep '^/root' > /dev/null || pwd | grep '^/sbin' > /dev/null || pwd | grep '^/bin' > /dev/null || pwd | grep '^/selinux' > /dev/null || pwd | grep '^/srv' > /dev/null || pwd | grep '^/usr' > /dev/null || pwd | grep '^/mnt' > /dev/null || pwd | grep '^/mount' > /dev/null || pwd | grep '^/media' > /dev/null || pwd | grep '^/dev' > /dev/null || pwd | grep '^/opt' > /dev/null || pwd | grep '^/bin' > /dev/null || pwd | grep '^/sys' > /dev/null || pwd | grep '^/lib64' > /dev/null || pwd | grep '^/proc' > /dev/null || pwd | grep '^/home$' > /dev/null || pwd | grep '^/var$' > /dev/null
+then
+	echo "Anti bumblebee security system engaged."
+	echo "Please execute the script in another directory, you were about to delete important files, and we don't want that :("
+	exit 1
+fi
+
+
 if [ "$1" = "-u" ] || [ "$1" = "--update" ]
 then
 	echo "Deleting old files...\c"
